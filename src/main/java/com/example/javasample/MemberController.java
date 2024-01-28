62ab8724d51b841e5a0695381c57c0bfdef2ba44
smrockv <smrockv@gmail.com>
2024-01-28 13:42:38 +0900

    とりあえずコントローラを追加

---
+++ src/main/java/com/example/javasample/MemberController.java
@@ -0,0 +1,12 @@
+package com.example.javasample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
