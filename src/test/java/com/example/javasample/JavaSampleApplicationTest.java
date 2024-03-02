47dcff6a344d8fd3319d77ea9ad0cbbeaf1c9519
smrockv <smrockv@gmail.com>
2024-03-02 16:06:37 +0900

    いろいろ反映

---
+++ src/test/java/com/example/javasample/JavaSampleApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.javasample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaSampleApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
